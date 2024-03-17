c05b16699979995aee5398b141ff9b54337d0e3a
matiasmunives <84053204+matiasmunives@users.noreply.github.com>
2024-03-16 22:18:02 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongodb = require('./data/database');
+const app = express();
+
+
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+if(err){
+  console.log(err);
+}
+else{
+  app.listen(port, () => {console.log(`Web Server is listening at port ${port}`)});
+}
+});
+
+
+
+
+
+
+ 
+
+
+
+
